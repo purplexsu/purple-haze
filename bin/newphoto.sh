8100599261850a346bd8c64c061fdae015996651
Purplexsu@gmail.com <Purplexsu@gmail.com>
2013-02-19 21:25:57 +0800

    Shell script to call Java app.

---
+++ bin/newphoto.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Author: purplexsu@gmail.com
+java -cp "/Users/frankxue/Projects/Pro052/ArticleGenerator.jar:/Users/frankxue/Projects/Pro052/jdom.jar:/Users/frankxue/Projects/Pro052/xercesImpl.jar:/Users/frankxue/Projects/Pro052/xml-apis.jar:/Users/frankxue/Projects/Pro052/commons-net-1.4.1.jar:/Users/frankxue/Projects/Pro052/commons-httpclient-3.1.jar:/Users/frankxue/Projects/Pro052/commons-codec-1.3.jar:/Users/frankxue/Projects/Pro052/commons-logging-1.1.1.jar" com.purplehaze.PhotoGenerator "/Users/frankxue/My Webs/PurpleHaze" "/Users/frankxue/My Webs/PurpleTemplate"
