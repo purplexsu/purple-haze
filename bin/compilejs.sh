d2d48eecf3286679c6e3c4e7f1b42eeac0508b67
Purplexsu@gmail.com <Purplexsu@gmail.com>
2013-02-28 21:35:56 +0800

    enable debug mode of compilejs.sh; call upload.py after compiling.

--- bin/compilejs.sh
+++ bin/compilejs.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 # Author: purplexsu
 
-function compile_js() {
-  project_dir="${HOME}/Projects/Pro052"
-  web_dir="${HOME}/My Webs"
+project_dir="${HOME}/Projects/Pro052"
+web_dir="${HOME}/My Webs"
 
+function compile_js() {
   python ${project_dir}/closure-library/closure/bin/build/closurebuilder.py \
     --root=${project_dir}/closure-library/ \
     --root=${project_dir}/ArticleGenerator/javascript/ \
     --namespace="$1" \
     --compiler_jar="${project_dir}/compiler.jar" \
     --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
-    --output_mode=compiled \
+    --output_mode="$3" \
     --output_file="${web_dir}/PurpleHaze/$2"
 }
 
-compile_js "net.purplexsu.ArticlePage" "article.js"
-compile_js "net.purplexsu.PhotoPage" "photo.js"
-compile_js "net.purplexsu.CommentPage" "comment.js"
+output_mode="compiled"
+if [ $# -gt 0 -a "$1" == "debug" ]
+then
+  output_mode="script"
+fi
+
+compile_js "net.purplexsu.ArticlePage" "article.js" "${output_mode}"
+compile_js "net.purplexsu.PhotoPage" "photo.js" "${output_mode}"
+compile_js "net.purplexsu.CommentPage" "comment.js" "${output_mode}"
+
+if [ "${output_mode}" != "script" ]
+then
+  read -p "Upload?[y|n] " -n 1
+  if [ $REPLY == "y" ]
+  then
+    python ${project_dir}/ArticleGenerator/bin/upload.py --type j
+  fi
+fi
+
